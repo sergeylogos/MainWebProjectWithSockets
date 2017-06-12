ce13a935cf5c3b82e6b6a5473dfeaa7aa458a679
sergeylogos <sergey.logos@gmail.com>
2017-06-12 20:08:54 +0300

    add filter utf-8
    add filter

--- src/main/java/ua/com/owu/configs/Security.java
+++ src/main/java/ua/com/owu/configs/Security.java
@@ -59,6 +59,10 @@ public class Security extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+         CharacterEncodingFilter filter = new CharacterEncodingFilter();
+         filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+         http.addFilterBefore(filter,CsrfFilter.class);
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/admin/**").access("hasRole('ADMIN')")
